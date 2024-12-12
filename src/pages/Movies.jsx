a49bc39c97d30f1ca11f7bfd3905c9a03f5506b3
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-12-12 23:40:23 +0100

    import pages

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,13 @@
+// pages/Movies.js
+import React from "react";
+
+const Movies = () => {
+  return (
+    <div>
+      <h1>Movies page</h1>
+      {/* Add your movies page content here */}
+    </div>
+  );
+};
+
+export default Movies;
